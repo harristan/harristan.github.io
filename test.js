36fe4632cd380a5ecff9b83eb97cf1f3a9fe1f2d
harristan <tankengsheng@hotmail.com>
2018-04-30 15:39:11 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){		
+
+	//// Force cached pages to reload
+	window.onpageshow = function(event) {
+			
+  		if (event.persisted) {
+        	window.location.reload();
+		}
+
+		$("html,body").scrollTop(0);
+	};
+
+	// Event handler for dropdown button proper styling
+	$(".p-profile").on("click", ".dropdown-toggle", function(event) {
+		var jqBtn = $(this);
+		jqBtn.css("background-color", "#f7f7f7");
+		jqBtn.css("color", "#337ab7");
+		jqBtn.css("border-color", "#337ab7");
+	});
+
+	// Ensures carousel does not automatically cle
+	$('.carousel').carousel({
+		interval: false
+	});
+
+}); // jQuery
